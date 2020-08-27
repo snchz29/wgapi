79e7c166e0358b3f8aa63ed96b9048d21cbfd35f
snchz29 <sasha4888.terehov@gmail.com>
2020-08-27 18:18:19 +0700

    delete url builder from clans

--- Clans.py
+++ Clans.py
@@ -4,65 +4,35 @@ import URLBuilder
 
 class Clans(MainAPI.MainAPI):
     def get_clans(self, fields=None, game=None, language='ru', limit=None, page_no=None, search=None):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/list')\
-            .set_application_id(self.__application_id)\
-            .add_parameter('fields', fields)\
-            .add_parameter('game', game)\
-            .add_parameter('language', language)\
-            .add_parameter('limit', limit)\
-            .add_parameter('page_no', page_no)\
-            .add_parameter('search', search)
-        return self.get_json(builder.url).json()
+        url = 'wgn/clans/list'
+        req = self._get_json(url, fields=fields, game=game, language=language, limit=limit, page_no=page_no,
+                             search=search)
+        return self._dump(req)
 
-    def get_details(self, clan_id, access_token=None, extra=None, fields=None, game=None, language='ru', members_key=None):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/info') \
-            .set_application_id(self.__application_id) \
-            .add_parameter('clan_id', clan_id) \
-            .add_parameter('__access_token', access_token) \
-            .add_parameter('extra', extra) \
-            .add_parameter('fields', fields) \
-            .add_parameter('game', game) \
-            .add_parameter('language', language)\
-            .add_parameter('members_key', members_key)
-        return self.get_json(builder.url).json()
+    def get_details(self, clan_id, access_token=None, extra=None, fields=None, game=None, language='ru',
+                    members_key=None):
+        url = 'wgn/clans/info'
+        req = self._get_json(url, clan_id=clan_id, access_token=access_token, extra=extra, fields=fields, game=game,
+                             language=language, members_key=members_key)
+        return self._dump(req)
 
     def get_member_details(self, account_id, fields=None, game=None, language='ru'):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/membersinfo') \
-            .set_application_id(self.__application_id) \
-            .add_parameter('account_id', account_id) \
-            .add_parameter('fields', fields) \
-            .add_parameter('game', game) \
-            .add_parameter('language', language)
-        return self.get_json(builder.url).json()
+        url = 'wgn/clans/membersinfo'
+        req = self._get_json(url, account_id=account_id, fields=fields, game=game, language=language)
+        return self._dump(req)
 
     def get_glossary(self, fields=None, game=None, language='ru'):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/glossary') \
-            .set_application_id(self.__application_id) \
-            .add_parameter('fields', fields) \
-            .add_parameter('game', game) \
-            .add_parameter('language', language)
-        return self.get_json(builder.url).json()
+        url = 'wgn/clans/glossary'
+        req = self._get_json(url, fields=fields, game=game, language=language)
+        return self._dump(req)
 
     def get_message_board(self, access_token=None, fields=None, game=None):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/messageboard') \
-            .set_application_id(self.__application_id) \
-            .add_parameter('__access_token', access_token) \
-            .add_parameter('fields', fields)\
-            .add_parameter('game', game)
-        return self.get_json(builder.url).json()
+        url = 'wgn/clans/messageboard'
+        req = self._get_json(url, access_token=access_token, fields=fields, game=game)
+        return self._dump(req)
 
     def get_history(self, account_id, fields=None, game=None, language=None):
-        builder = URLBuilder.URLBuilder('wgn')
-        builder.set_request_target('clans/memberhistory') \
-            .set_application_id(self.__application_id) \
-            .add_parameter('account_id', account_id) \
-            .add_parameter('fields', fields) \
-            .add_parameter('game', game)\
-        .add_parameter('language', language)
-        return self.get_json(builder.url).json()
+        url = 'wgn/clans/memberhistory'
+        req = self._get_json(url, fields=fields, game=game, language=language)
+        return self._dump(req)
 
